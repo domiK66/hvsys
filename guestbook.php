af3ae3b306557f1ed24030293c4d8bc9aed692f7
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2021-06-20 18:55:24 +0200

    bootstrap table

--- guestbook.php
+++ guestbook.php
@@ -15,51 +15,64 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
         <title>HVSYS secure webserver</title>
     </head>
     <body>
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <div class="container">
-                    <a class="navbar-brand" href="/">HVSYS Project</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="navbar-nav ml-auto">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container">
+                <a class="navbar-brand" href="/">HVSYS Project</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
                             <a class="nav-link" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                            <a class="nav-link" href="/guestbook.php">Guestbook</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Services</a>
+                            <a class="nav-link" href="/snake.html">Snake Game JS</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                        </ul>
-                    </div>
+                    </ul>
                 </div>
-            </nav>
-        </div>
-        <div class="container" style="margin-top: 5rem;">
-		<a href="add.html">Add New Data</a><br/><br/>
+            </div>
+        </nav>
 
-		<table width='80%' border=0>
-			<tr bgcolor='#CCCCCC'>
-				<td>Firstname</td>
-				<td>Lastname</td>
-				<td>Message</td>
-			</tr>
-			<?php 
-			while($res = mysqli_fetch_array($result)) { 		
-				echo "<tr>";
-				echo "<td>".$res['firstname']."</td>";
-				echo "<td>".$res['lastname']."</td>";
-				echo "<td>".$res['message']."</td>";	
-				echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-			}
-			?>
-			</table>
+        <div class="container" role="main">
+
+		    <a href="add.html" class="btn btn-xs btn-success">Add New Data</a><br/><br/>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <table data-toggle="table" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th data-sortable="true">Firstname</td>
+                                <th data-sortable="true">Lastname</td>
+                                <th data-sortable="true">Message</td>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                                while($res = mysqli_fetch_array($result)) { 		
+                                    echo "<tr>";
+                                    echo "<td>".$res['firstname']."</td>";
+                                    echo "<td>".$res['lastname']."</td>";
+                                    echo "<td>".$res['message']."</td>";	
+                                    echo "<td><a href=\"edit.php?id=$res[id]\" class="btn btn-xs btn-primary">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\" class="btn btn-xs btn-danger">Delete</a></td>";		
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            
+            <p class="mt-4">Project by Dominik Kainz, Mensur Bukarevic & Lukas Dworacek
+            </p>
+            <p>Copyright by <a href="https://www.fh-joanneum.at">www.fh-joanneum.at</a></p>
+
         </div>
-</body>
+
+
+    </body>
 </html>
