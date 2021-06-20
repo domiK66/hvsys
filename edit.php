bcdb99b33169d2b46072ae7a994b3f9d9b0c6d8d
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2021-06-20 19:23:47 +0200

    finished layout

--- edit.php
+++ edit.php
@@ -48,34 +48,63 @@ while($res = mysqli_fetch_array($result))
 	$email = $res['message'];
 }
 ?>
-<html>
-<head>	
-	<title>Edit Data</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>HVSYS Edit Data</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container">
+                <a class="navbar-brand" href="/">HVSYS Project</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/guestbook.php">Guestbook</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/snake.html">Snake Game JS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div class="container" role="main">
+			
+			<br/><br/>
 
-<body>
-	<a href="guestbook.php">Back</a>
-	<br/><br/>
-	
-	<form name="form1" method="post" action="edit.php">
-		<table border="0">
-			<tr> 
-				<td>Firstname</td>
-				<td><input type="text" name="firstname" value="<?php echo $firstname;?>"></td>
-			</tr>
-			<tr> 
-				<td>Lastname</td>
-				<td><input type="text" name="lastname" value="<?php echo $lastname;?>"></td>
-			</tr>
-			<tr> 
-				<td>Message</td>
-				<td><input type="text" name="email" value="<?php echo $message;?>"></td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
-			</tr>
-		</table>
-	</form>
-</body>
+			<form action="edit.php" method="post" name="form1">
+				<table width="25%" border="0">
+					<tr> 
+						<td>Firstname</td>
+						<td><input type="text" name="firstname" value="<?php echo $firstname;?>"></td>
+					</tr>
+					<tr> 
+						<td>Lastname</td>
+						<td><input type="text" name="lastname" value="<?php echo $lastname;?>"></td>
+					</tr>
+					<tr> 
+						<td>Message</td>
+						<td><input type="text" name="email" value="<?php echo $message;?>"></td>
+					</tr>
+					<tr>
+						<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+						<td><input type="submit" name="update" value="Update"></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</body>
 </html>
+
+
