0d425c2ea57ef5d6f6819e04c3d0ed4b12517cb6
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2021-06-20 18:57:16 +0200

    fix

--- guestbook.php
+++ guestbook.php
@@ -66,13 +66,14 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
                 </div>
             </div>
 
-            
+
             <p class="mt-4">Project by Dominik Kainz, Mensur Bukarevic & Lukas Dworacek
             </p>
             <p>Copyright by <a href="https://www.fh-joanneum.at">www.fh-joanneum.at</a></p>
 
         </div>
 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
     </body>
 </html>
